faf3b8d539ae9fb3e03650fc4ada9d4b744da05d
Sharif <sharifhosen73@gmail.com>
2022-11-08 23:24:16 +0600

    get data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.v5n2r.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const placeCollection = client.db("tourist-services").collection("places");
+    app.get("/place", async (req, res) => {
+      const query = {};
+      const cursor = placeCollection.find(query);
+      const places = await cursor.toArray();
+      res.send(places);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("touriest services server");
+  res.send("tourist services server");
 });
 
 app.listen(port, () => {
-  console.log("touriest services server", port);
+  console.log("tourist services server", port);
 });
