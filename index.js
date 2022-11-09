b3cdacceb21c07d272d816a6b620453e451e20b6
Sharif <sharifhosen73@gmail.com>
2022-11-09 16:41:34 +0600

    delete api added

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
+
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
